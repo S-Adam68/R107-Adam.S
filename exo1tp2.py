128f5c7c4a9c1fa93cedc485a797bb95e7ad8294
e2500544 <adam.sebar@uha.fr>
2025-11-06 15:54:06 +0100

    test

--- exo1tp2.py
+++
@@ -1,7 +0,0 @@
-x= int(input("Entrez x : "))
-y= int(input("Entrez x : "))
-
-z=x
-x=y
-y=z
-print(x,y)
--- exo2tp2.py
+++
@@ -1,4 +0,0 @@
-age= int(input("Donné votre age : " ))
-naissance= 2025-age
-print("Votre anneé de naissance est: ", naissance)
-
